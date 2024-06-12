0d6cc1d56650e8e6453e532a9997320f91d82da9
flowercordoba <flowercordoba7@gmail.com>
2024-06-12 13:30:23 -0500

    setup project base

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
+
+import { Application } from 'express';
+
+
+const BASE_PATH = '/api/v1';
+
+export default (app: Application) => {
+  const routes = () => {
+  
+
+  };
+  routes();
+};
